f624e8a39201d368fde820cb52210a140fd78caf
Berk Gercek <gercek.berk@gmail.com>
2018-05-21 09:22:47 +0200

    Update to 2 sigma case

--- codes/dynamic_adhoc_twosigma.py
+++ codes/dynamic_adhoc_twosigma.py
@@ -6,8 +6,13 @@ Ad hoc model with differing sigma for the present and absent cases
 
 import numpy as np
 import matplotlib.pyplot as plt
+import multiprocessing as mulpro
+import itertools as it
 import seaborn as sbn
+import time
 from scipy.optimize import brentq
+import pickle
+import os
 
 T = 10
 t_w = 0.5
@@ -27,8 +32,10 @@ def f(x, g_t, sigma):
 def p_new_ev(x, g_t, sigma):
     ''' The probability of a given observation x_(t+1) given our current belief
     g_t'''
-    p_pres = np.exp(- (x - 1)**2 / (2 * sigma[1]**2)) / np.sqrt(2 * np.pi * sigma[1]**2)
-    p_abs = np.exp(- x**2 / (2 * sigma[0]**2)) / np.sqrt(2 * np.pi * sigma[0]**2)
+    p_pres = np.exp(- (x - 1)**2 /
+                    (2 * sigma[1]**2)) / np.sqrt(2 * np.pi * sigma[1]**2)
+    p_abs = np.exp(- x**2 / (2 * sigma[0]**2)) / \
+        np.sqrt(2 * np.pi * sigma[0]**2)
     return p_pres * g_t + p_abs * (1 - g_t)
 
 
@@ -80,8 +87,10 @@ def main(argvec):
         for j in range(size):
             g_tp1 = g_values[j]
             try:
-                rootgrid[i, j, 0] = brentq(lambda x: g_tp1 - f(x, g_t, sigma), -150, ourpeak)
-                rootgrid[i, j, 1] = brentq(lambda x: g_tp1 - f(x, g_t, sigma), ourpeak, 150)
+                rootgrid[i, j, 0] = brentq(
+                    lambda x: g_tp1 - f(x, g_t, sigma), -150, ourpeak)
+                rootgrid[i, j, 1] = brentq(
+                    lambda x: g_tp1 - f(x, g_t, sigma), ourpeak, 150)
             except ValueError:
                 if g_t > g_tp1:
                     rootgrid[i, j, 0] = -150
@@ -96,13 +105,17 @@ def main(argvec):
 
     # Decision values are static for a given g_t and independent of t. We compute these
     # in advance
-    decision_vals = np.zeros((size, 2))  # N x 2 matrix. First column is resp. abs, second is pres.
-    decision_vals[:, 1] = g_values * R[1, 1] + (1 - g_values) * R[1, 0]  # respond present
-    decision_vals[:, 0] = (1 - g_values) * R[0, 0] + g_values * R[0, 1]  # respond absent
+    # N x 2 matrix. First column is resp. abs, second is pres.
+    decision_vals = np.zeros((size, 2))
+    decision_vals[:, 1] = g_values * R[1, 1] + \
+        (1 - g_values) * R[1, 0]  # respond present
+    decision_vals[:, 0] = (1 - g_values) * R[0, 0] + \
+        g_values * R[0, 1]  # respond absent
 
     # Create array to store V for each g_t at each t. N x (T / dt)
     V_full = np.zeros((size, int(T / dt)))
-    V_full[:, -1] = np.max(decision_vals, axis=1)  # At large T we assume val of waiting is zero
+    # At large T we assume val of waiting is zero
+    V_full[:, -1] = np.max(decision_vals, axis=1)
 
     # Corresponding array to store the identity of decisions made
     decisions = np.zeros((size, int(T / dt)))
@@ -115,28 +128,53 @@ def main(argvec):
 
         for i in range(size):
             g_t = g_values[i]  # Pick ith value of g at t
-            roots = rootgrid[i, :, :]  # Slice roots of our given g_t across all g_(t+1)
-            new_g_probs = p_new_ev(roots, g_t, sigma)  # Find the likelihood of roots x_(t+1)
+            # Slice roots of our given g_t across all g_(t+1)
+            roots = rootgrid[i, :, :]
+            # Find the likelihood of roots x_(t+1)
+            new_g_probs = p_new_ev(roots, g_t, sigma)
             new_g_probs = new_g_probs / np.sum(new_g_probs)  # Normalize
             new_g_probs = np.sum(new_g_probs, axis=1)  # Sum across both roots
-            V_wait = np.sum(new_g_probs * V_full[:, -(index - 1)]) - rho * t  # Sum and subt op cost
+            # Sum and subt op cost
+            V_wait = np.sum(new_g_probs * V_full[:, -(index - 1)]) - rho * t
 
             # Find the maximum value b/w waiting and two decision options. Store value and identity.
-            V_full[i, -index] = np.amax((V_wait, decision_vals[i, 0], decision_vals[i, 1]))
-            decisions[i, -index] = np.argmax((V_wait, decision_vals[i, 0], decision_vals[i, 1]))
+            V_full[i, -index] = np.amax((V_wait,
+                                         decision_vals[i, 0], decision_vals[i, 1]))
+            decisions[i, -index] = np.argmax((V_wait,
+                                              decision_vals[i, 0], decision_vals[i, 1]))
 
     # Simulate a pool of observers
-    # pool = mulpro.Pool(processes=8)
-    # C_vals = [0, 1] * 200
-    # arglists = it.product(C_vals, [decisions])
-    # observer_outputs = pool.map(simulate_observer, arglists)
     numsims = 2000
+    pool = mulpro.Pool(processes=8)
     C_vals = [0] * numsims
     C_vals.extend([1] * numsims)
-    observer_outputs = []
-    for C in C_vals:
-        observer_outputs.append(simulate_observer([C, decisions, sigma, dt]))
+    arglists = it.product(C_vals, [decisions], [sigma], [dt])
+    observer_outputs = pool.map(simulate_observer, arglists)
 
     g_grid = np.array([x[2] for x in observer_outputs])
     response_times = np.array([x[1] for x in observer_outputs])
     return g_grid, response_times, decisions
+
+
+if __name__ == '__main__':
+    rho = 0.05
+    reward = 3
+    punishment = -.1
+    dt = 0.02
+    multiple_trials = {}
+    sigmas = np.linspace(0.9, 10, 10)
+    raw_pairs = list(it.product(sigmas, sigmas))
+    sigma_list = [x for x in raw_pairs if x[0] != x[1]]
+    for sigma in sigma_list:
+        multiple_trials[sigma] = main([dt, np.array(sigma), rho, reward, punishment])
+
+    currtime = time.localtime()
+    filename = os.getenv("HOME") + '/Documents/sigma_param_search_{}_{}_{}'.format(currtime.tm_mday,
+                                                                                   currtime.tm_mon,
+                                                                                   currtime.tm_year)
+    fw = open(filename, 'wb')
+    outdict = {'trials_dict': multiple_trials, 'T': T, 'dt': dt, 't_w': t_w, 'size': size,
+               'g_values': g_values, 'rho': rho, 'reward': reward, 'punishment': punishment}
+    pickle.dump(outdict, fw)
+    fw.close()
+
