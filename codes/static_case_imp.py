c9bf94cc30b43ceebdaacc1e07dc5cd00376211b
Reidar Riveland <reidar.riveland@gmail.com>
2018-06-05 15:03:14 +0200

    Update static_case_imp.py

--- codes/static_case_imp.py
+++ codes/static_case_imp.py
@@ -13,8 +13,7 @@ sigma = 1
 
 
 # values that grid points might take
-value_space = np.linspace(10**-3, 1 - 10**-3, size)
-
+grid_space = np.linspace(10**-3, 1 - 10**-3, size)
 
 def combs(a, r):
     """
@@ -24,10 +23,16 @@ def combs(a, r):
     """
     return np.array(list(product(a, repeat=r)))
 
+grid_values = combs(grid_space, 4)
 
-grid_values = combs(value_space, 4)
-
-
+def norm_draw(C, x):
+    if C==1:
+        return np.exp(-(x - 1)**2 / (2 * sigma**2))
+    if C==0: 
+        return np.exp(-(x)**2 / (2 * sigma**2))
+    else: 
+        raise ValueError('C is a binary variable and must take value 0 or 1')
+    
 def global_posterior(Phi, k):
     '''
     this is g_t in the write up
@@ -47,7 +52,7 @@ def local_posterior(Phi, k):
 
     pres_likelihood = phi * beta_bar
     Z_b = phi * beta_bar + phi_bar * beta + \
-        (N - k) * phi_bar * beta_bar  # this is the normalizing factor
+    (N - k) * phi_bar * beta_bar  # this is the normalizing factor
 
     return pres_likelihood / Z_b
 
@@ -61,10 +66,7 @@ def p_new_ev_stay(x, Phi, sigma, k):
     g_t = global_posterior(Phi, k)
     b_t = local_posterior(Phi, k)
 
-    draw_1 = np.exp(-(x - 1)**2 / (2 * sigma**2))
-    draw_0 = np.exp(-x**2 / (2 * sigma**2))
-
-    return (1 - g_t) * draw_0 + g_t * (b_t * draw_1 + (1 - b_t) * draw_0)
+    return (1 - g_t) * norm_draw(1, x) + g_t * (b_t * norm_draw(1, x) + (1 - b_t) * norm_draw(0, x))
 
 
 def p_new_ev_switch(x, Phi, sigma, k):
@@ -77,16 +79,13 @@ def p_new_ev_switch(x, Phi, sigma, k):
 
     Z_b = phi * beta_bar + phi_bar * beta + (N - k) * phi_bar * beta_bar
 
-    draw_1 = np.exp(-(x - 1)**2 / (2 * sigma**2))
-    draw_0 = np.exp(-x**2 / (2 * sigma**2))
-
     # this is the bit of the equation that captures weights the draws on local post
     # conditioned on target present
     # mostly just for notational convience
-    weight_draw_pres = ((phi_bar * beta_bar) / Z_b) * draw_1 + \
-        ((phi * beta_bar + phi_bar * beta + (N - (k - 1)) * phi_bar * beta_bar) / Z_b) * draw_0
+    weight_draw_pres = ((phi_bar * beta_bar) / Z_b) * norm_draw(1, x) + \
+        ((phi * beta_bar + phi_bar * beta + (N - (k - 1)) * phi_bar * beta_bar) / Z_b) * norm_draw(0, x)
 
-    return (1 - g_t) * draw_0 + g_t * (weight_draw_pres)
+    return (1 - g_t) * norm_draw(0, x) + g_t * (weight_draw_pres)
 
 
 def get_Update_X(Phi_t):
@@ -100,18 +99,19 @@ def get_Update_X(Phi_t):
     phi_bar_t = Phi_t[1]
     ##An initial matrix of root values to be computed for each potential 
     ##phi_tp1 and phi_bar_tp1 pair, later to be expanded for size^4 space
-    init_roots = np.full((size**2,2), 150)
+    init_roots = np.full((size**2,2), np.NaN)
     ##phi, phi_bar pairs 
-    phi_phi_bar_space = combs(value_space, 2)
+    phi_phi_bar_space = combs(grid_space, 2)
     
     for i in range(size):
         #we look at possible values of phi_tp1 
-        phi_tp1 = value_space[i]
+        #!!!!!change the name
+        phi_tp1 = grid_space[i]
         # the get values of X such that Phi_t is updated to each potential new Phi_t+1
         try:
-            root_1 = brentq(lambda x: phi_tp1 - np.exp(-(x - 1)**2 / (2 * sigma**2)) * phi_t,
+            root_1 = brentq(lambda x: phi_tp1 - norm_draw(1, x) * phi_t,
                                   -150, 1)  # root finding
-            root_2 = brentq(lambda x: phi_tp1 - np.exp(-(x - 1)**2 / (2 * sigma**2)) * phi_t,
+            root_2 = brentq(lambda x: phi_tp1 - norm_draw(1, x) * phi_t,
                                   1, 150)  # root finding
         except ValueError:
             if phi_t > phi_tp1:
@@ -122,35 +122,88 @@ def get_Update_X(Phi_t):
                 root_2 = 150
         #using the root values for phi_tp1, we find the correspondent value 
         #of phi_bar_tp1 that would result from an update with the same root
-        phi_bar_tp_1 = value_space[np.abs(value_space-(root_1*phi_bar_t)).argmin()]
-        phi_bar_tp_2 = value_space[np.abs(value_space-(root_2*phi_bar_t)).argmin()]
+        phi_bar_tp_1 = grid_space[np.abs(grid_space-(norm_draw(0, root_1)*phi_bar_t)).argmin()]
+        phi_bar_tp_2 = grid_space[np.abs(grid_space-(norm_draw(0, root_2)*phi_bar_t)).argmin()]
         #based on the aquired phi_bar_tp1, we match the root_1
         # with the proper phi_tp1, phi_bar_tp1 pair 
         for j in range(size):
             if phi_phi_bar_space[(size*i)+j][1] == phi_bar_tp_1:
                 init_roots[(size*i)+j][0] = root_1
-            elif phi_phi_bar_space[(size*i)+j][1] == phi_bar_tp_2:
+            if phi_phi_bar_space[(size*i)+j][1] == phi_bar_tp_2:
                 init_roots[(size*i)+j][1] = root_2
 
     roots = np.c_[phi_phi_bar_space, init_roots]
-
-
+    # full_update = np.zeros((size**4, 2))
+    # for i in range(size**2): 
+    #     roots = init_roots[i, :]
+    #     for j in range(size**2):
+    #         full_update[(size*i)+j] = roots
     return roots
 
 updates = get_Update_X((0.25, 0.25, 0.25, 0.25))
-print(updates)
+updates.shape
 
+def get_Update_X_test(Phi_t):
+    '''
+    takes a Phi and returns a size**4 vector of x's that update current Phi to future Phi
+    it should be noted that updates only apply to the phi's and so the same update vector
+    can be used for all Phi with given phi, phi_bar
+    '''
+    #the current phi values based on location in the grid
+    phi_t = Phi_t[0]
+    phi_bar_t = Phi_t[1]
+    ##An initial matrix of root values to be computed for each potential 
+    ##phi_tp1 and phi_bar_tp1 pair, later to be expanded for size^4 space
+    init_roots = np.full((size**2,2), np.NaN)
+    ##phi, phi_bar pairs 
+    phi_phi_bar_space = combs(grid_space, 2)
+    
+    for i in range(size):
+        #we look at possible values of phi_tp1 
+        phi_bar_tp1 = grid_space[i]
+        # the get values of X such that Phi_t is updated to each potential new Phi_t+1
+        try:
+            root_1 = brentq(lambda x: phi_bar_tp1 - norm_draw(0, x) * phi_bar_t,
+                                  -150, 0)  # root finding
+            root_2 = brentq(lambda x: phi_bar_tp1 - norm_draw(0, x) * phi_bar_t,
+                                  0, 150)  # root finding
+        except ValueError:
+            if phi_bar_t > phi_bar_tp1:
+                root_1 = -150
+                root_2 = -150
+            elif phi_bar_t < phi_bar_tp1:
+                root_1 = 150
+                root_2 = 150
+        #using the root values for phi_tp1, we find the correspondent value 
+        #of phi_bar_tp1 that would result from an update with the same root
+        phi_tp_1 = grid_space[np.abs(grid_space-(norm_draw(1, root_1)*phi_t)).argmin()]
+        phi_tp_2 = grid_space[np.abs(grid_space-(norm_draw(1, root_2)*phi_t)).argmin()]
+        #based on the aquired phi_bar_tp1, we match the root_1
+        # with the proper phi_tp1, phi_bar_tp1 pair 
+        for j in range(size):
+            if phi_phi_bar_space[(size*i)+j][1] == phi_tp_1:
+                init_roots[(size*i)+j][0] = root_1
+            if phi_phi_bar_space[(size*i)+j][1] == phi_tp_2:
+                init_roots[(size*i)+j][1] = root_2
+
+    roots = np.c_[phi_phi_bar_space, init_roots]
+    return roots
+
+update_test = get_Update_X_test((0.8, 0.7, 0.25, 0.25))
 
 
 
 def root_checker(phi, phi_bar, roots_arr):
-    checks = np.zeros((size**2, 2))
+    checks = np.full(size**2, False)
     for i in range(size**2):
-        pres_check = np.exp(-(roots_arr[i][2] - 1)**2 / (2 * sigma**2)) * phi
-        abs_check = np.exp(-(roots_arr[i][2])**2 / (2 * sigma**2)) * phi_bar
-        checks[i][0] = pres_check
-        checks[i][1] = abs_check
-    checks = np.c_[checks, roots_arr]
+        if np.isnan(roots_arr[i,3]):
+            checks[i] = True
+        else:
+            phi_check = norm_draw(1, roots_arr[i,3]) * phi
+            phi_bar_check = norm_draw(0, roots_arr[i,3]) * phi_bar
+            checks[i] =  
+            
+    checks = np.c_[roots_arr[:, 0:3], checks]
     return checks
 
 def main(argvec):
@@ -218,3 +271,4 @@ if __name__ == '__main__':
     dt = 0.05
     sigma = 15
 #    grid, times, decisions = main([dt, sigma, rho, reward, punishment])
+
