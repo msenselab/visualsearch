a29557e0dcb999efb87080fcc99ff5d2aad56deb
Reidar Riveland <reidar.riveland@gmail.com>
2018-07-25 13:21:29 +0200

    fixes to likelihood

--- codes/dyn_case_multi_model.py
+++ codes/dyn_case_multi_model.py
@@ -15,6 +15,7 @@ import pandas as pd
 from pathlib import Path
 import pickle
 from gauss_opt import bayesian_optimisation
+from dynamic_adhoc_twosigma import posterior
 import numpy as np
 
 # Returns a path object that works as a string for most functions
@@ -25,7 +26,7 @@ savepath = str(savepath.expanduser())
 T = 10
 t_w = 0.5
 size = 100
-g_values = np.linspace(1 / size, 1 - 1 / size, size)
+g_values = np.linspace(1/size, 1 - 1/size, size)
 d_map_samples = int(1e5)
 dt = 0.05
 N_array = [8, 12, 16]
@@ -49,7 +50,6 @@ exp1.rename(columns={'sub': 'subno'}, inplace=True)
 temp = np.mean(np.array(exp1['rt']))
 sub_data = exp1.query('subno == {} & dyn == \'Dynamic\''.format(subject_num))
 
-
 def d_map(N, epsilons, sigma_N):
     '''
     Computes the decisions variable d based on the log likelihood ratio
@@ -58,7 +58,6 @@ def d_map(N, epsilons, sigma_N):
     '''
     return -(1 / (2 * sigma_N**2)) + np.log(1 / N) + \
         np.log(np.sum(np.exp(epsilons / sigma_N**2)))
-
 
 def sample_epsilon(C, N, sigma):
     '''
@@ -69,7 +68,6 @@ def sample_epsilon(C, N, sigma):
     if C == 1:
         epsilons[0] = norm.rvs(1, sigma)
     return epsilons
-
 
 def get_coarse_stats(fine_sigma, num_samples, model_type):
     '''
@@ -86,7 +84,7 @@ def get_coarse_stats(fine_sigma, num_samples, model_type):
         if model_type == 'const':
             sigma_N = fine_sigma
         if model_type == 'sqrt':
-            sigma_N = fine_sigma * (np.sqrt(N) / np.sqrt(N_min))
+            sigma_N = fine_sigma * (np.sqrt(N)/np.sqrt(N_min))
         pres_samples = np.zeros(num_samples)
         abs_samples = np.zeros(num_samples)
         for j in range(num_samples):
@@ -98,40 +96,35 @@ def get_coarse_stats(fine_sigma, num_samples, model_type):
 
     return stats
 
-
 def g_to_D(g_t):
-    return np.log(g_t / (1 - g_t))
-
+    return np.log(g_t/(1-g_t))
 
 def D_to_g(D_t):
-    return np.exp(D_t) / (1 + np.exp(D_t))
-
+    return np.exp(D_t)/(1+np.exp(D_t))
 
 def deriv_dg_dD(D_t):
-    return D_to_g(D_t) * (1 - D_to_g(D_t))
-
+    return D_to_g(D_t)*(1-D_to_g(D_t))
 
 def p_gtp1_gt(g_t, g_tp1, sigma, mu):
     D_t = g_to_D(g_t)
     D_tp1 = g_to_D(g_tp1)
-    jacobian_factor = 1 / deriv_dg_dD(D_t)
+    jacobian_factor = 1/deriv_dg_dD(D_t)
 
-    pres_draw = g_t * norm.pdf(D_tp1, D_t + mu[1], sigma[1])
-    abs_draw = (1 - g_t) * norm.pdf(D_tp1, D_t + mu[0], sigma[0])
+    pres_draw = g_t*norm.pdf(D_tp1, D_t+mu[1], sigma[1])
+    abs_draw = (1-g_t)*norm.pdf(D_tp1, D_t+mu[0], sigma[0])
 
-    return jacobian_factor * (pres_draw + abs_draw)
-
+    return jacobian_factor*(pres_draw+abs_draw)
 
 def p_Dtp1_Dt(D_t, D_tp1, sigma, mu):
     g_t = D_to_g(D_t)
 
-    pres_draw = g_t * norm.pdf(D_tp1, D_t + mu[1], sigma[1])
-    abs_draw = (1 - g_t) * norm.pdf(D_tp1, D_t + mu[0], sigma[0])
+    pres_draw = g_t*norm.pdf(D_tp1, D_t+mu[1], sigma[1])
+    abs_draw = (1-g_t)*norm.pdf(D_tp1, D_t+mu[0], sigma[0])
 
-    return pres_draw + abs_draw
+    return pres_draw+abs_draw
 
 
-def trans_probs(sigma, mu, space='g'):
+def trans_probs(sigma, mu, space = 'g'):
     if space == 'g':
         dg = g_values[1] - g_values[0]
         prob_grid = np.zeros((size, size))
@@ -143,7 +136,7 @@ def trans_probs(sigma, mu, space='g'):
     if space == 'D':
         D_values = np.linspace(0, 1e2, 1e3)
         dD = D_values[1] - D_values[0]
-        prob_grid = np.zeros((len(D_values), len(D_values)))
+        prob_grid = np.zeros((len(D_values),len(D_values)))
         for i, D_t in enumerate(D_values):
             updates = p_Dtp1_Dt(D_t, D_values, sigma, mu)
             updates = updates / (np.sum(updates) * dD)
@@ -153,7 +146,7 @@ def trans_probs(sigma, mu, space='g'):
 
 
 def back_induct(reward, punishment, rho, sigma, mu, prob_grid, reward_scheme,
-                t_dependent=False):
+    t_dependent = False):
     dg = g_values[1] - g_values[0]
 
     # Define the reward array
@@ -189,25 +182,23 @@ def back_induct(reward, punishment, rho, sigma, mu, prob_grid, reward_scheme,
     for index in range(2, int(T / dt) + 1):
         for i in range(size):
             V_wait = np.sum(prob_grid[:, i] * V_full[:, -(index - 1)]) * dg - (rho * dt)
-            # Find the maximum value b/w waiting and two decision options. Store value and identity.
+            #Find the maximum value b/w waiting and two decision options. Store value and identity.
             V_full[i, -index] = np.amax((V_wait, decision_vals[i, 0], decision_vals[i, 1]))
             decisions[i, -index] = np.argmax((V_wait, decision_vals[i, 0], decision_vals[i, 1]))
         if not t_dependent and index > 20:
-            absolute_err = np.abs(V_full[:, -index] - V_full[:, -(index - 1)])
+            absolute_err = np.abs(V_full[:, -index] - V_full[:, -(index-1)])
             converged = np.all(absolute_err[5:-5] < 1e-5)
             if converged:
                 dec_vec = decisions[:, -index]
-                # deal with degenerate cases in which there are no 1, 2s
+                #deal with degenerate cases in which there are no 1, 2s
                 dec_vec[0] = 1
                 dec_vec[-1] = 2
                 abs_threshold = np.amax(np.where(dec_vec == 1)[0])
                 pres_threshold = np.where(dec_vec == 2)[0][0]
                 dec_vec[0:abs_threshold] = 1
                 dec_vec[pres_threshold:len(dec_vec)] = 2
-                V_full = np.reshape(
-                    np.repeat(V_full[:, -index], V_full.shape[1]), (size, V_full.shape[1]))
-                decisions = np.reshape(
-                    np.repeat(dec_vec, decisions.shape[1]), (size, decisions.shape[1]))
+                V_full = np.reshape(np.repeat(V_full[:, -index], V_full.shape[1]), (size, V_full.shape[1]))
+                decisions = np.reshape(np.repeat(dec_vec, decisions.shape[1]), (size, decisions.shape[1]))
                 break
             if index == int(T / dt):
                 print('!!!backward induction did not converge to fixed point!!!')
@@ -222,15 +213,15 @@ def solve_rho(reward, punishment, reward_scheme, sigma, mu, prob_grid):
     '''
     def V_in_rho(log_rho):
         rho = np.exp(log_rho)
-        values = back_induct(reward, punishment, rho, sigma, mu,
-                             prob_grid, reward_scheme)[0]
+        values = back_induct(reward, punishment, rho, sigma, mu, \
+            prob_grid, reward_scheme)[0]
         return values[int(size / 2), 0]
 
     # when optimizing for reward this optimization should be accounted for in choosing bounds
     try:
         opt_log_rho = brentq(V_in_rho, -10 + np.log(reward), 10 + np.log(reward))
     except ValueError:
-        raise Exception("defective bounds in rho finding procedure")
+            raise Exception("defective bounds in rho finding procedure")
 
     return np.exp(opt_log_rho)
 
@@ -238,34 +229,35 @@ def solve_rho(reward, punishment, reward_scheme, sigma, mu, prob_grid):
 def simulate_observer(arglist):
     C, decisions, sigma, mu, dt = arglist
 
-    dec_vec = decisions[:, 0]
+    dec_vec = decisions[:,0]
     abs_bound = g_values[np.amax(np.where(dec_vec == 1)[0])]
     pres_bound = g_values[np.where(dec_vec == 2)[0][0]]
 
     D_t = 0
     t = 0
 
-    g_trajectory = np.ones(int(T / dt)) * 0.5
-    D_trajectory = np.zeros(int(T / dt))
+    g_trajectory = np.ones(int(T/dt))*0.5
+    D_trajectory = np.zeros(int(T/dt))
 
     while t < T:
         if C == 1:
-            D_t = norm.rvs(t * mu[C], t * sigma[C]) * dt
+            D_t = norm.rvs(t * mu[C], t* sigma[C]) * dt
         if C == 0:
-            D_t = norm.rvs(t * mu[C], t * sigma[C]) * dt
+            D_t = norm.rvs(t * mu[C], t*sigma[C]) * dt
 
         g_t = D_to_g(D_t)
-        D_trajectory[int(t / dt)] = D_t
-        g_trajectory[int(t / dt)] = g_t
+        D_trajectory[int(t/dt)] = D_t
+        g_trajectory[int(t/dt)] = g_t
         t += dt
 
         if g_t < abs_bound:
-            return (1, t, g_trajectory, D_trajectory)
+            return (0, t, g_trajectory, D_trajectory)
 
         if g_t > pres_bound:
-            return (2, t, g_trajectory, D_trajectory)
+            return (1, t, g_trajectory, D_trajectory)
 
     return (0, T, g_trajectory, D_trajectory)
+
 
 
 # def alt_sim(prob_grid, decisions):
@@ -314,19 +306,52 @@ def simulate_observer(arglist):
 #     return resp_abs, resp_pres, dist_evo
 
 
-def get_rt(sigma, mu, decisions):
-    numsims = 2000
+def get_rt(sigma, mu, decisions, numsims = 5000):
     C_vals = [0] * numsims
     C_vals.extend([1] * numsims)
     arglists = it.product(C_vals, [decisions], [sigma], [mu], [dt])
     observer_outputs = []
     for arglist in arglists:
         observer_outputs.append(simulate_observer(arglist))
-    response_times = np.array([x[1] for x in observer_outputs])
-    return response_times.reshape(2, numsims)
+    response_info = np.array([(x[0], x[1]) for x in observer_outputs])
+    abs_info = response_info[:numsims,:]
+    pres_info = response_info[numsims:,:]
+
+    return (abs_info, pres_info)
 
 
 def get_single_N_likelihood(data, sim_rt, reward):
+
+    #simulate response times for C = 0
+    abs_rt = sim_rt[0]
+    #simulate response times for C = 1
+    pres_rt = sim_rt[1]
+
+    # Simulate model distribution for resp = 0, C = 0
+    abs_0_sim_rt = np.array(abs_rt[np.where(abs_rt[:,0] == 0)[0]])[:,1]
+    abs_0_sim_rt_dist = gaussian_kde(abs_0_sim_rt, bw_method=0.1)
+
+    # Simulate model distribution for resp = 1, C = 1
+    pres_1_sim_rt = np.array(pres_rt[np.where(pres_rt[:,0] == 1)[0]])[:,1]
+    pres_1_sim_rt_dist = gaussian_kde(pres_1_sim_rt, bw_method=0.1)
+
+    # Simulate model distribution for resp = 1, C = 0
+    if np.all(abs_rt[:, 0] == True):
+        # filler distribution as frac_pres_cor will eval to 0
+        abs_1_sim_rt = np.random.uniform()
+    else:
+        abs_1_sim_rt = np.array(abs_rt[np.where(abs_rt[:,0] == 1)[0]])[:,1]
+    abs_1_sim_rt_dist = gaussian_kde(abs_1_sim_rt, bw_method=0.1)
+
+    # Simulate model distribution for resp = 0, C = 1
+    if np.all(pres_rt[:, 0] == True):
+        # filler distribution as frac_pres_inc will eval to 0
+        pres_0_sim_rt = np.random.uniform()
+    else:
+        pres_0_sim_rt = np.array(pres_rt[np.where(pres_rt[:,0] == 0)[0]])[:,1]
+
+    pres_0_sim_rt_dist = gaussian_kde(pres_0_sim_rt, bw_method=0.1)
+
 
     pres_rts_0 = data.query('resp == 2 & target == \'Present\'').rt.values
     pres_rts_1 = data.query('resp == 1 & target == \'Present\'').rt.values
@@ -334,43 +359,28 @@ def get_single_N_likelihood(data, sim_rt, reward):
     abs_rts_0 = data.query('resp == 2 & target == \'Absent\'').rt.values
     abs_rts_1 = data.query('resp == 1 & target == \'Absent\'').rt.values
 
-    # deal with the cases where sim rt are all the same giving 0 var to KDE
-    if np.var(sim_rt[1, :]) == 0:
-        mean = np.mean(sim_rt[1, :])
-        perturb = np.random.normal(mean, 0.01)
-        sim_rt[1, 0] = mean + perturb
-
-    pres_sim_rt_dist = gaussian_kde(sim_rt[1, :], bw_method=0.1)
-
-    if np.var(sim_rt[0, :]) == 0:
-        mean = np.mean(sim_rt[0, :])
-        perturb = np.random.normal(mean, 0.1)
-        sim_rt[0, 0] = mean + perturb
-
-    abs_sim_rt_dist = gaussian_kde(sim_rt[0, :], bw_method=0.1)
 
     frac_pres_inc = len(pres_rts_0) / (len(pres_rts_0) + len(pres_rts_1))
     frac_pres_corr = len(pres_rts_1) / (len(pres_rts_0) + len(pres_rts_1))
     log_like_pres = np.concatenate((np.log(frac_pres_inc) +
-                                    np.log(pres_sim_rt_dist.pdf(pres_rts_0)),
+                                    np.log(pres_0_sim_rt_dist.pdf(pres_rts_0)),
                                     np.log(frac_pres_corr) +
-                                    np.log(pres_sim_rt_dist.pdf(pres_rts_1))))
+                                    np.log(pres_1_sim_rt_dist.pdf(pres_rts_1))))
 
     frac_abs_inc = len(abs_rts_1) / (len(abs_rts_0) + len(abs_rts_1))
     frac_abs_corr = len(abs_rts_0) / (len(abs_rts_0) + len(abs_rts_1))
     log_like_abs = np.concatenate((np.log(frac_abs_corr) +
-                                   np.log(abs_sim_rt_dist.pdf(abs_rts_0)),
+                                   np.log(abs_0_sim_rt_dist.pdf(abs_rts_0)),
                                    np.log(frac_abs_inc) +
-                                   np.log(abs_sim_rt_dist.pdf(abs_rts_1))))
+                                   np.log(abs_1_sim_rt_dist.pdf(abs_rts_1))))
 
     log_like_all = np.concatenate((log_like_pres, log_like_abs))
 
     likelihood_pertrial = (1 - lapse) * np.exp(log_like_all) + (lapse / 2) * np.exp(-reward / temp)
     return -np.sum(np.log(likelihood_pertrial))
 
-
 def get_data_likelihood(sub_data, log_reward, log_punishment, log_sigma,
-                        reward_scheme, fine_model_type):
+                                        reward_scheme, fine_model_type):
     sigma = np.exp(log_sigma)
     reward = np.exp(log_reward)
     punishment = -np.exp(log_punishment)
@@ -387,12 +397,11 @@ def get_data_likelihood(sub_data, log_reward, log_punishment, log_sigma,
         probs = trans_probs(sigma, mu)
         rho = solve_rho(reward, punishment, reward_scheme, sigma, mu, probs)
         decisions = back_induct(reward, punishment, rho, sigma, mu,
-                                probs, reward_scheme)[1]
+                                                probs, reward_scheme)[1]
         sim_rt = get_rt(sigma, mu, decisions)
         likelihood += get_single_N_likelihood(data[i], sim_rt, reward)
 
     return likelihood
-
 
 if __name__ == '__main__':
 
@@ -419,11 +428,10 @@ if __name__ == '__main__':
     if model_type[0] == 'sig':
         reward_scheme = model_type[1]
         fine_model_type = model_type[2]
-
         def subject_likelihood(params):
             log_sigma = params[0]
             return get_data_likelihood(sub_data, 0, -1e5, log_sigma,
-                                       reward_scheme, fine_model_type)
+                reward_scheme, fine_model_type)
 
         bnds = np.array(((-1.7, 1.),))  # [n_variables, 2] shaped array with bounds
         x_opt = bayesian_optimisation(n_iters=iter_bayesian_opt, sample_loss=subject_likelihood,
@@ -431,12 +439,11 @@ if __name__ == '__main__':
     if model_type[0] == 'sig_reward':
         reward_scheme = model_type[1]
         fine_model_type = model_type[2]
-
         def subject_likelihood(params):
             log_sigma = params[0]
             log_reward = params[1]
-            return get_data_likelihood(sub_data, log_reward, -1e5, log_sigma,
-                                       reward_scheme, fine_model_type)
+            return get_data_likelihood( sub_data, log_reward, -1e5, log_sigma,
+                reward_scheme, fine_model_type)
 
         bnds = np.array(((-1.7, 1.), (-5., 0.5)))  # [n_variables, 2] shaped array with bounds
         x_opt = bayesian_optimisation(n_iters=iter_bayesian_opt, sample_loss=subject_likelihood,
@@ -445,12 +452,11 @@ if __name__ == '__main__':
     if model_type[0] == 'sig_punish':
         reward_scheme = model_type[1]
         fine_model_type = model_type[2]
-
         def subject_likelihood(params):
             log_sigma = params[0]
             log_punishment = params[1]
             return get_data_likelihood(sub_data, 0, log_punishment, log_sigma,
-                                       reward_scheme, fine_model_type)
+                reward_scheme, fine_model_type)
 
         bnds = np.array(((-1.7, 1.), (-5., -0.5)))  # [n_variables, 2] shaped array with bounds
         x_opt = bayesian_optimisation(n_iters=iter_bayesian_opt, sample_loss=subject_likelihood,
@@ -459,6 +465,7 @@ if __name__ == '__main__':
     xp, yp = x_opt
     # Pull out each of the log(sigma) that the optimizer tested and put them in an array together
     # with the associated log(likelihood). datarr is (N x 2) where N is the number of optimize samps
+
 
     # Plot test points and likelihoods
     fig = plt.figure()
@@ -471,7 +478,7 @@ if __name__ == '__main__':
         ax.set_ylabel('$log(reward)$')
         ax.set_zlabel('$log(likelihood)$')
 
-    if model_type[0] == 'sig_punish':
+    if  model_type[0] == 'sig_punish':
         ax.scatter(xp[:, 0], xp[:, 1], yp, s=100)
         ax.set_xlabel('$log(\sigma)$')
         ax.set_ylabel('$log(punishment)$')
@@ -497,30 +504,31 @@ if __name__ == '__main__':
         reward = 1
         punishment = 0
         fig.suptitle('Parameters: sigma = {}'.format(np.round(best_sigma, 3))
-                     + ', Reward Scheme: {},'.format(model_type[1])
-                     + ' Fine Model: {}'.format(model_type[2]))
+                            + ', Reward Scheme: {},'.format(model_type[1]) \
+                            + ' Fine Model: {}'.format(model_type[2]))
     elif model_type[0] == 'sig_reward':
         reward = np.exp(best_params[1])
         punishment = 0
         fig.suptitle('Parameters: sigma = {}'.format(np.round(best_sigma, 3))
-                     + '; reward = {}'.format(np.round(reward, 3))
-                     + ', Reward Scheme: {},'.format(model_type[1])
-                     + ' Fine Model: {}'.format(model_type[2]))
+                            + '; reward = {}'.format(np.round(reward, 3))
+                            + ', Reward Scheme: {},'.format(model_type[1]) \
+                            + ' Fine Model: {}'.format(model_type[2]))
     elif model_type[0] == 'sig_punish':
         punishment = np.exp(best_params[1])
         reward = 1
         fig.suptitle('Parameters: sigma = {}'.format(np.round(best_sigma, 3))
-                     + '; punishment = {}'.format(np.round(punishment))
-                     + ', Reward Scheme: {},'.format(model_type[1])
-                     + ' Fine Model: {}'.format(model_type[2]))
+                            + '; punishment = {}'.format(np.round(punishment))
+                            + ', Reward Scheme: {},'.format(model_type[1]) \
+                            + ' Fine Model: {}'.format(model_type[2]))
 
     data_array = [sub_data.query('setsize == 8'), sub_data.query('setsize == 12'),
-                  sub_data.query('setsize == 16')]
+            sub_data.query('setsize == 16')]
 
     all_rt = {}
     display_con = ('pres', 'abs')
     for n_con in it.product(N_array, display_con):
         all_rt[n_con] = []
+
 
     stats = get_coarse_stats(best_sigma, d_map_samples, model_type[2])
 
@@ -560,7 +568,7 @@ if __name__ == '__main__':
 
     plt.savefig(savepath + '/subject_{}_bayes_opt_bestfits.png'.format(subject_num))
 
-    fw = open(savepath + '/subject_{}_simrt_and_params.p'.format(subject_num), 'wb')
+    fw = open(savepath + '/subject_{}_simrt_and_params.p', 'wb')
     outdict = {'best_sigma': best_params[0], 'best_reward': best_params[1], 'sim_rts': all_rt,
                'coarse_stats': stats}
     pickle.dump(outdict, fw)
